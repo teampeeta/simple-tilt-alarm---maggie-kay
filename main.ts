dced954e38276e40d201363952ebb842919d9aba
teampeeta <152302872+teampeeta@users.noreply.github.com>
2023-12-11 10:55:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Angry)
+    music.play(music.stringPlayable("G A B B B A G F ", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+	
+})
